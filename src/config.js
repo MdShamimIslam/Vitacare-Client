0ffae350348af96ce163653a29115632bba8d79f
MdShamimIslam <shamim401897@gmail.com>
2024-12-06 15:39:00 +0600

    added backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,2 @@
 
-
-// export const BASE_URL = "https://backend-cyan-pi.vercel.app/api/v1";
-export const BASE_URL = "http://localhost:5000/api/v1";
-export const token = localStorage.getItem("token");
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
